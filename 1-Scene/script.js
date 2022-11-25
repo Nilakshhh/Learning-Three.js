294a021858a90d077f4638c48bfd754066ea412a
Nilakshhh <dnilaksh05@gmail.com>
2022-11-25 12:16:28 +0530

    modify

--- 1-Scene/script.js
+++ 1-Scene/script.js
@@ -1,4 +1,4 @@
-const scene = new THREE.Scene();
+/*const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color: 'red'});
@@ -20,3 +20,32 @@ const renderer = THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+
+*/
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xb4ea2d
+});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 1;
+
+var animate = function() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.05;
+  cube.rotation.y -= 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
